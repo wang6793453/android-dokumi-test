7963b2ad6549bced63540e332f9b80ff102c3a14
wangyf <wang6793453@gmail.com>
2017-09-22 11:40:49 +0900

    新規作成

---
+++ app/src/main/java/com/example/wangyf/android_dokumi_test/Main2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.wangyf.android_dokumi_test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
